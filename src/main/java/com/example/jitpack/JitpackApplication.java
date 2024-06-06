168db877b815cb8f4aa616554275f546731558b8
Felipe Albino Gomes <felipe.gomes.ext@spms.min-saude.pt>
2024-06-06 07:38:20 -0300

    Fist commit

---
+++ src/main/java/com/example/jitpack/JitpackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jitpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JitpackApplication.class, args);
+	}
+
+}
