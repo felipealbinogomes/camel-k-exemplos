f44780909237e8079be44ca5d3381bd243e592e3
Felipe Albino Gomes <felipe.gomes.ext@spms.min-saude.pt>
2024-06-11 07:06:57 -0300

    [fix] - Fixed ProduceCxf class to suporte config endpoint with no serviceClass

---
+++ src/main/java/com/example/jitpack/cxf/ProduceCxf.java
@@ -0,0 +1,28 @@
+package com.example.jitpack.cxf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
+import org.apache.camel.component.cxf.common.DataFormat;
+
+
+@Configuration
+public class ProduceCxf {
+
+    //    @Bean
+//    public CxfEndpoint producerCxf() throws ClassNotFoundException {
+//        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+//        cxfEndpoint.setAddress("http://localhost:8090/soap/hl7/patientId");
+//        cxfEndpoint.setWsdlURL("http://localhost:8090/soap/hl7/patientId?wsdl");
+//    //    cxfEndpoint.setServiceClass("PatientIdService");
+//        return cxfEndpoint;
+//    }
+    @Bean
+    public CxfEndpoint producerCxf() throws ClassNotFoundException {
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setAddress("http://localhost:8090/soap/hl7/patientId");
+        cxfEndpoint.setWsdlURL("http://localhost:8090/soap/hl7/patientId?wsdl");
+        cxfEndpoint.setDataFormat(DataFormat.MESSAGE);
+        return cxfEndpoint;
+    }
+}
